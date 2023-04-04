9d7948ac9c7ca843a600761960749279a57a82ad
lironcp <lironcpp@gmail.com>
2023-04-04 05:13:04 -0700

    GUI Updated

--- scripts/GUI.py
+++ scripts/GUI.py
@@ -15,7 +15,7 @@ class StdoutRedirector(object):
 
     def write(self, message):
         self.text_space.insert(tk.END, message)
-
+    
 def get_water_temperature(): # for water temeperature graph
     # Read data from the JSON file
     with open("water_temp.json", "r") as infile:
@@ -42,6 +42,42 @@ def get_water_temperature(): # for water temeperature graph
 
     # Show the plot
     plt.show()
+
+def generate_values():
+    global dog_house_temp, dog_house_humidity, water_temp, food_weight, dog_weight
+    dog_house_temp = round(random.uniform(10, 30), 2)
+    dog_house_humidity = round(random.uniform(20, 80), 2)
+    water_temp = round(random.uniform(5, 15), 2)
+    food_weight = round(random.uniform(100, 500), 2)
+    dog_weight = round(random.uniform(5, 50), 2)
+
+    # Update the GUI elements to display the new values
+    # temp_humid_label.config(text=f"Currently DogHouse Temperature: {dog_house_temp}°C, Humidity: {dog_house_humidity}%")
+    # water_temp_label.config(text=f"Water Temperature: {water_temp}")
+    # food_weight_label.config(text=f"Food Weight: {food_weight}")
+    # dog_weight_label.config(text=f"Dog Weight: {dog_weight}")
+
+
+def dog_house_temp_gen():
+     print(f"Currently DogHouse Temperature: {dog_house_temp}°C, Humidity: {dog_house_humidity}%" )
+     generate_values()
+def water_temp_gen():
+     print(f"Water Temperature: {water_temp}°C" )
+     generate_values()
+def food_weight_gen():
+     print(f"Food Tank Weight: {food_weight}Kg" )
+     generate_values()
+def dog_weight_gen():
+     print(f"Dog Weight: {dog_weight}Kg")
+     generate_values()
+
+
+
+dog_house_temp = round(random.uniform(10, 30), 2)
+dog_house_humidity = round(random.uniform(20, 80), 2)
+water_temp = round(random.uniform(5, 15), 2)
+food_weight = round(random.uniform(100, 500), 2)
+dog_weight = round(random.uniform(5, 50), 2)
 
 # Generate 20 measurements for "Water Temperature" metric
 
@@ -71,7 +107,7 @@ root.configure(bg="white")  # Change background color to white
 
 root2 = tk.Tk() #For outputs
 root2.title("GUI outputs")
-root2.geometry("400x200")
+root2.geometry("600x300")
 
 text_widget = tk.Text(root2)
 text_widget.pack() 
@@ -92,7 +128,6 @@ background_label = tk.Label(root, image=photo)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 
-
 # Define styles for labels and buttons
 label_style = {"font": ("Arial", 16, "bold"), "fg": "black", "bg": "white"}  # Change label color to black and background color to white
 button_style = {"font": ("Arial", 12), "fg": "white", "bg": "#0074D9"}  # Change button color to blue
@@ -100,26 +135,27 @@ label_style2 = {"font": ("Arial", 16, "bold"), "fg": "black"}
 # Define the labels and buttons
 temp_humid_label = tk.Label(root, text="DogHouse Temperature & Humidity", **label_style)
 temp_humid_label.pack(pady=10)
-temp_humid_button = tk.Button(root, text="Check", command=lambda: print("Currently DogHouse Temperature & Humidity"), **button_style)
+temp_humid_button = tk.Button(root, text="Check", command=dog_house_temp_gen, **button_style)
 temp_humid_button.pack()
 
 water_temp_label = tk.Label(root, text="Water Temperature", **label_style)
 water_temp_label.pack(pady=10)
-water_temp_button = tk.Button(root, text="Check", command=lambda: print("Currently Water Temperature"), **button_style)
+water_temp_button = tk.Button(root, text="Check", command=water_temp_gen, **button_style)
 water_temp_button.pack()
 
 food_weight_label = tk.Label(root, text="Food Weight", **label_style)
 food_weight_label.pack(pady=10)
-food_weight_button = tk.Button(root, text="Check", command=lambda: print("Food Weight"), **button_style)
+food_weight_button = tk.Button(root, text="Check",command= food_weight_gen, **button_style)
 food_weight_button.pack()
 
 
 
 dog_weight_label = tk.Label(root, text="Dog Weight", **label_style)
 dog_weight_label.pack(pady=10)
-dog_weight_button = tk.Button(root, text="Check", command=lambda: print("Dog Weight"), **button_style)
+dog_weight_button = tk.Button(root, text="Check", command= dog_weight_gen, **button_style)
 dog_weight_button.pack()
 
+generate_button = tk.Button(root, text="Generate Values", command=generate_values) 
 
 
 row1 = tk.Frame(root)
@@ -151,10 +187,13 @@ plot_label.pack(pady=60)
 
 row1.pack()
 row2.pack()
+generate_button.pack()
 
 # Define the plot window for the graphs
 fig = Figure(figsize=(6, 4), dpi=100)
 plot_window = FigureCanvasTkAgg(fig, root)
 plot_window.get_tk_widget().pack(padx=20, pady=10, side=tk.BOTTOM, fill=tk.BOTH, expand=1)
 
+#if(not(root2.winfo_exists())):
+ #   root2 = tk.Tk() #For outputs
 root.mainloop()

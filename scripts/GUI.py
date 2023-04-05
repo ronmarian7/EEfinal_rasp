707dba0f66a1ef0aec01e7c8d82b6bf0d114f062
lironcp <lironcpp@gmail.com>
2023-04-05 02:37:27 -0700

    gui updated

--- scripts/GUI.py
+++ scripts/GUI.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter.ttk import Style
 from PIL import Image, ImageTk
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -15,8 +16,87 @@ class StdoutRedirector(object):
 
     def write(self, message):
         self.text_space.insert(tk.END, message)
-    
-def get_water_temperature(): # for water temeperature graph
+
+def plot_food_consuming():
+    with open("dog_water_consumption.json", "r") as f:
+        data = json.load(f)
+
+    # Create a list of x and y values for the plot
+    x_values = list(data.keys())
+    y_values = list(data.values())
+
+    # Plot the data as a line graph
+    plt.figure()
+    plt.plot(x_values, y_values)
+    plt.xlabel("Day of the week")
+    plt.ylabel("Water consumption (liters)")
+    plt.title("Dog water consumption over time")
+    plt.show()
+
+def plot_water_consuming():
+    with open("dog_food_consumption.json", "r") as f:
+       data = json.load(f)
+
+    # Create a list of x and y values for the plot
+    x_values = list(data.keys())
+    y_values = list(data.values())
+
+    # Plot the data as a line graph
+    plt.figure()
+    plt.plot(x_values, y_values)
+    plt.xlabel("Day of the week")
+    plt.ylabel("Food consumption (kilograms)")
+    plt.title("Dog food consumption over time")
+    plt.show()
+
+def plot_humidity() :
+    with open("dog_humidity.json", "r") as f:
+        data = json.load(f)
+
+    # Create a list of x and y values for the plot
+    x_values = list(data.keys())
+    y_values = list(data.values())
+
+    # Plot the data as a line graph
+    plt.figure()
+    plt.plot(x_values, y_values)
+    plt.xlabel("Day of the week")
+    plt.ylabel("Dog House Humidity")
+    plt.title("DogHouse Humidity Over Time")
+    plt.show()
+
+def plot_dog_weight(): # for dog weight graph
+    with open("dog_weights.json", "r") as f:
+        data = json.load(f)
+    # Create a list of x and y values for the plot
+    x_values = list(data.keys())
+    y_values = list(data.values())
+
+    # Plot the data as a line graph
+    plt.figure()
+    plt.plot(x_values, y_values)
+    plt.xlabel("Day")
+    plt.ylabel("Dog Weight")
+    plt.title("Dog Weight Over Time")
+    plt.show()
+
+def plot_doghouse_temepratrue(): # for doghouse temeperature 
+    with open("dog_temperatures.json", "r") as f:
+       data = json.load(f)
+
+    # Create a list of x and y values for the plot
+    x_values = list(data.keys())
+    y_values = list(data.values())
+
+    # Plot the data as a line graph
+    plt.figure()
+    plt.plot(x_values, y_values)
+    plt.xlabel("Day of the week")
+    plt.ylabel("Dog house temperature")
+    plt.title("DogHouse Temperature Over Time")
+    plt.show()
+
+def plot_water_temperature(): # for water temeperature graph
     # Read data from the JSON file
     with open("water_temp.json", "r") as infile:
         data = json.load(infile)
@@ -47,15 +127,10 @@ def generate_values():
     global dog_house_temp, dog_house_humidity, water_temp, food_weight, dog_weight
     dog_house_temp = round(random.uniform(10, 30), 2)
     dog_house_humidity = round(random.uniform(20, 80), 2)
+    water_weight = round(random.uniform(0, 15), 2)
     water_temp = round(random.uniform(5, 15), 2)
-    food_weight = round(random.uniform(100, 500), 2)
+    food_weight = round(random.uniform(0, 20), 2)
     dog_weight = round(random.uniform(5, 50), 2)
-
-    # Update the GUI elements to display the new values
-    # temp_humid_label.config(text=f"Currently DogHouse Temperature: {dog_house_temp}°C, Humidity: {dog_house_humidity}%")
-    # water_temp_label.config(text=f"Water Temperature: {water_temp}")
-    # food_weight_label.config(text=f"Food Weight: {food_weight}")
-    # dog_weight_label.config(text=f"Dog Weight: {dog_weight}")
 
 
 def dog_house_temp_gen():
@@ -65,22 +140,23 @@ def water_temp_gen():
      print(f"Water Temperature: {water_temp}°C" )
      generate_values()
 def food_weight_gen():
-     print(f"Food Tank Weight: {food_weight}Kg" )
+     print(f"Food Tank Weight: {food_weight}Kg, Water Tank Weight: {water_weight}L" )
      generate_values()
 def dog_weight_gen():
      print(f"Dog Weight: {dog_weight}Kg")
      generate_values()
 
-
-
 dog_house_temp = round(random.uniform(10, 30), 2)
 dog_house_humidity = round(random.uniform(20, 80), 2)
 water_temp = round(random.uniform(5, 15), 2)
-food_weight = round(random.uniform(100, 500), 2)
+food_weight = round(random.uniform(0, 20), 2)
 dog_weight = round(random.uniform(5, 50), 2)
+water_weight = round(random.uniform(0, 20), 2)
 
-# Generate 20 measurements for "Water Temperature" metric
 
+#######################         DATA GENERATING        ###############################
+
+# Generate 20 measurements for "Water Temperature" Graph
 data = []
 start_date = datetime.datetime(2023, 2, 1)
 end_date = datetime.datetime(2023, 5, 31)
@@ -90,14 +166,49 @@ for i in range(20):
     date = start_date + datetime.timedelta(days=random.randint(0, (end_date - start_date).days))
     measurement = {"date": date.strftime("%Y-%m-%d %H:%M:%S"), "value": temperature}
     data.append(measurement)
-
 data = sorted(data, key=lambda x: x['date'])
+
+# Generate data for "Dog Weight" Graph
+weights = {}
+days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+for day in days:
+    weights[day] = round(random.uniform(30, 35), 2)
+
+# Generate data for "Doghouse Temeperature"
+temperatures = {}
+for day in days:
+    temperatures[day] = round(random.uniform(5, 35), 2)
+
+# Generate dog house humidity data
+humidity = {}
+for day in days:
+    humidity[day] = round(random.uniform(20, 80), 2)
+
+# Generate dog food consumption data
+food_consumption = {}
+for day in days:
+    food_consumption[day] = round(random.uniform(0.5, 2.5), 2)
+
+# Generate dog water consumption data
+water_consumption = {}
+for day in days:
+    water_consumption[day] = round(random.uniform(0.5, 2.5), 2)
 
 
 # Write data to a JSON file
 with open("water_temp.json", "w") as outfile:
     json.dump(data, outfile)
-    
+with open("dog_weights.json", "w") as f:
+    json.dump(weights, f)
+with open("dog_temperatures.json", "w") as f:
+    json.dump(temperatures, f)
+with open("dog_humidity.json", "w") as f:
+    json.dump(humidity, f)
+with open("dog_water_consumption.json", "w") as f:
+    json.dump(water_consumption, f)
+with open("dog_food_consumption.json", "w") as f:
+    json.dump(food_consumption, f)
+
 # Define the GUI window
 root = tk.Tk()
 root.title("Smart Dog House - GUI")
@@ -127,7 +238,6 @@ photo = ImageTk.PhotoImage(image)
 background_label = tk.Label(root, image=photo)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-
 # Define styles for labels and buttons
 label_style = {"font": ("Arial", 16, "bold"), "fg": "black", "bg": "white"}  # Change label color to black and background color to white
 button_style = {"font": ("Arial", 12), "fg": "white", "bg": "#0074D9"}  # Change button color to blue
@@ -143,7 +253,7 @@ water_temp_label.pack(pady=10)
 water_temp_button = tk.Button(root, text="Check", command=water_temp_gen, **button_style)
 water_temp_button.pack()
 
-food_weight_label = tk.Label(root, text="Food Weight", **label_style)
+food_weight_label = tk.Label(root, text="Food & Water Tank`s Weight", **label_style)
 food_weight_label.pack(pady=10)
 food_weight_button = tk.Button(root, text="Check",command= food_weight_gen, **button_style)
 food_weight_button.pack()
@@ -162,23 +272,23 @@ row1 = tk.Frame(root)
 row2 = tk.Frame(root)
 
 # pack the first row of buttons
-weight_graph_button = tk.Button(row1, text="Dog Weight Graph", command=lambda: print("Weight Graph"), **button_style)
+weight_graph_button = tk.Button(row1, text="Dog Weight Graph", command=lambda: plot_dog_weight(), **button_style)
 weight_graph_button.pack(side='left', padx=0, pady=0)
 
-temp_graph_button = tk.Button(row1, text="DogHouse Temperature Graph", command=lambda: print("Temperature Graph"), **button_style)
+temp_graph_button = tk.Button(row1, text="DogHouse Temperature Graph", command=lambda: plot_doghouse_temepratrue(), **button_style)
 temp_graph_button.pack(side='left', padx=0, pady=0)
 
-hum_graph_button = tk.Button(row1, text="DogHouse Humidity Graph", command=lambda: print("Humidity Graph"), **button_style)
+hum_graph_button = tk.Button(row1, text="DogHouse Humidity Graph", command=lambda:plot_humidity(), **button_style)
 hum_graph_button.pack(side='left', padx=0, pady=0)
 
 # pack the second row of buttons
-water_graph_button = tk.Button(row2, text="Water Consuming Graph", command=lambda: print("Water Graph"), **button_style)
+water_graph_button = tk.Button(row2, text="Water Consuming Graph", command=lambda: plot_water_consuming(), **button_style)
 water_graph_button.pack(side='left', padx=0, pady=0.5)
 
-food_graph_button = tk.Button(row2, text="Food Consuming Graph", command=lambda: print("Food Graph"), **button_style)
+food_graph_button = tk.Button(row2, text="Food Consuming Graph", command=lambda: plot_food_consuming(), **button_style)
 food_graph_button.pack(side='left', padx=0, pady=0.5)
 
-temp_graph_button = tk.Button(row2, text="Water Temperature Graph", command=lambda: get_water_temperature(), **button_style)
+temp_graph_button = tk.Button(row2, text="Water Temperature Graph", command=lambda: plot_water_temperature(), **button_style)
 temp_graph_button.pack(side='left', padx=0, pady=0.5)
 
 # pack the rows of buttons into the main window
@@ -187,7 +297,6 @@ plot_label.pack(pady=60)
 
 row1.pack()
 row2.pack()
-generate_button.pack()
 
 # Define the plot window for the graphs
 fig = Figure(figsize=(6, 4), dpi=100)

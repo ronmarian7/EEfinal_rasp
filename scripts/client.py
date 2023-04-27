668d8885e31073daa0bed5d925d3708a929b3bd9
lironcp <lironcpp@gmail.com>
2023-04-27 19:59:37 +0300

    Updated client with the requirements handling

--- scripts/client.py
+++ scripts/client.py
@@ -4,6 +4,7 @@ import os
 from datetime import datetime
 import queue
 import threading
+import tkinter as tk
 
 """
 TODOs
@@ -51,19 +52,33 @@ class ClientSocket:
         if self.dog_weight > 2:  # threshold for the dog weight
             self.avg_dog_weight = ((self.avg_dog_weight * self.num_samples) + self.dog_weight) / (self.num_samples + 1)
             self.num_samples += 1
+    
+
+    def exceeding_requirements_printer(req_exceeded: str):
+        root = tk.Tk()
+        root.title("Alert")
+        root.geometry("250x100")
+    
+        msg = tk.Label(root, text=f"{req_exceeded} Exceeded the requirements")
+        msg.pack(pady=20)
+        # TODO: make some sound alert also!
+        ok_button = tk.Button(root, text="OK", command=root.destroy)
+        ok_button.pack(pady=10)
+    
+        root.mainloop()
 
     def check_data(self):
         # Check if the values meet out requirements
         # FIXME need to see if we need to check the values of the other sensors also
         if self.dog_weight > 2:
             if not (0.9 * self.avg_dog_weight <= self.dog_weight <= 1.1 * self.avg_dog_weight):
-                pass
-        if not (10 <= self.dog_house_temp <= 50):  # FIXME - weight of the doghouse
-            pass
+                self.exceeding_requirements_printer("Dog Weight")
+        if not (10 <= self.dog_house_temp <= 40):  # FIXME - weight of the doghouse
+            self.exceeding_requirements_printer("Dog House Temperature")
         if not (0.5 <= self.water_weight):  # FIXME - weight of the dispense
-            pass
+            self.exceeding_requirements_printer("Water Weight")
         if not (3 <= self.food_weight):  # FIXME - weight of the dispenser
-            pass
+            self.exceeding_requirements_printer("Food Weight")
 
     def print_data(self):
         print("Time - {}  dog_house_temp: {:.1f} C    dog_house_humidity: {}%  dog_weight: {}KG    food_weight: {}gr\
